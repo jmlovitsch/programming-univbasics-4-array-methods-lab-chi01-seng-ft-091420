1c402208a033ca28faa7fbcd47d13d1df1be82e4
John Lovitsch <jmlovitsch@gmail.com>
2020-08-26 20:59:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,12 @@
 def using_include(array, element)
-
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array.include?("wow")
 end
 
 def using_sort(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.last 
+  array.first 
 end
 
 def using_reverse(array)
